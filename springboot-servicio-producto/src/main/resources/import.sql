03a1d60a729a8a69e1225a561679bc8e86299ef5
Marcelo Núñez <marcelo1nunez@gmail.com>
2021-01-12 22:27:59 -0300

    first commmit

---
+++ springboot-servicio-producto/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO productos (nombre, precio, created_at) VALUES ('Panasonic', 800, NOW());
+INSERT INTO productos (nombre, precio, created_at) VALUES ('Sony',700, NOW());
+INSERT INTO productos (nombre, precio, created_at) VALUES ('Apple',1000, NOW());
